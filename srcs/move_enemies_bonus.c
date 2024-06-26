92c228224d21919948db8c8da491a0f8f2738fa0
timofeykafanov <tim.kafanov@yandex.ru>
2024-06-26 17:47:19 +0200

    refactor: fix randomizer

--- srcs/move_enemies_bonus.c
+++ srcs/move_enemies_bonus.c
@@ -6,11 +6,40 @@
 /*   By: tkafanov <tkafanov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 16:32:07 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/06/26 17:00:29 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/06/26 17:40:39 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+static void choose_direction(char **map, int *i, int *j)
+{
+	int	num;
+
+	num = rand() % 4;
+	if (num == 0 && map[*i][*j + 1] == FLOOR)
+	{
+		map[*i][*j] = FLOOR;
+		map[*i][*j + 1] = ENEMY;
+		j++;
+	}
+	else if (num == 1 && map[*i + 1][*j] == FLOOR)
+	{
+		map[*i][*j] = FLOOR;
+		map[*i + 1][*j] = ENEMY;
+		i++;
+	}
+	else if (num == 2 && map[*i - 1][*j] == FLOOR)
+	{
+		map[*i][*j] = FLOOR;
+		map[*i - 1][*j] = ENEMY;
+	}
+	else if (num == 3 && map[*i][*j - 1] == FLOOR)
+	{
+		map[*i][*j] = FLOOR;
+		map[*i][*j - 1] = ENEMY;
+	}
+}
 
 void	move_enemies(char **map)
 {
@@ -25,30 +54,7 @@ void	move_enemies(char **map)
 		while (map[i][j])
 		{
 			if (map[i][j] == ENEMY)
-			{
-				if (rand() % 4 == 0 && map[i][j + 1] == FLOOR)
-				{
-					map[i][j] = FLOOR;
-					map[i][j + 1] = ENEMY;
-					j++;
-				}
-				else if (rand() % 4 == 1 &&map[i + 1][j] == FLOOR)
-				{
-					map[i][j] = FLOOR;
-					map[i + 1][j] = ENEMY;
-					i++;
-				}
-				else if (rand() % 4 == 2 &&map[i - 1][j] == FLOOR)
-				{
-					map[i][j] = FLOOR;
-					map[i - 1][j] = ENEMY;
-				}
-				else if (rand() % 4 == 3 &&map[i][j - 1] == FLOOR)
-				{
-					map[i][j] = FLOOR;
-					map[i][j - 1] = ENEMY;
-				}
-			}
+				choose_direction(map, &i, &j);
 			j++;
 		}
 		i++;
