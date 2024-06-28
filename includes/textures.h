1c76007fcd1b3ee052e4cb6328b00a7106620940
timofeykafanov <tim.kafanov@gmail.com>
2024-06-28 09:43:11 +0200

    refactor: separate headers

---
+++ includes/textures.h
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkafanov <tkafanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 09:35:31 by tkafanov          #+#    #+#             */
+/*   Updated: 2024/06/28 09:36:37 by tkafanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEXTURES_H
+# define TEXTURES_H
+
+# define P_IMG_0 "./textures/hero/sprite_0.xpm"
+
+# define P_IMG_1_L "./textures/hero/sprite_1_l.xpm"
+# define P_IMG_2_L "./textures/hero/sprite_2_l.xpm"
+# define P_IMG_3_L "./textures/hero/sprite_3_l.xpm"
+
+# define P_IMG_1_U "./textures/hero/sprite_1_u.xpm"
+# define P_IMG_2_U "./textures/hero/sprite_2_u.xpm"
+# define P_IMG_3_U "./textures/hero/sprite_3_u.xpm"
+
+# define P_IMG_1_R "./textures/hero/sprite_1_r.xpm"
+# define P_IMG_2_R "./textures/hero/sprite_2_r.xpm"
+# define P_IMG_3_R "./textures/hero/sprite_3_r.xpm"
+
+# define P_IMG_1_D "./textures/hero/sprite_1_d.xpm"
+# define P_IMG_2_D "./textures/hero/sprite_2_d.xpm"
+# define P_IMG_3_D "./textures/hero/sprite_3_d.xpm"
+
+# define ENEMY_RED_0 "./textures/enemy/red/red_0.xpm"
+# define ENEMY_RED_1 "./textures/enemy/red/red_1.xpm"
+# define ENEMY_RED_2 "./textures/enemy/red/red_2.xpm"
+
+# define ENEMY_GREEN_0 "./textures/enemy/green/green_0.xpm"
+# define ENEMY_GREEN_1 "./textures/enemy/green/green_1.xpm"
+# define ENEMY_GREEN_2 "./textures/enemy/green/green_2.xpm"
+
+# define ENEMY_SKY_0 "./textures/enemy/sky/sky_0.xpm"
+# define ENEMY_SKY_1 "./textures/enemy/sky/sky_1.xpm"
+# define ENEMY_SKY_2 "./textures/enemy/sky/sky_2.xpm"
+
+# define ENEMY_PURPLE_0 "./textures/enemy/purple/purple_0.xpm"
+# define ENEMY_PURPLE_1 "./textures/enemy/purple/purple_1.xpm"
+# define ENEMY_PURPLE_2 "./textures/enemy/purple/purple_2.xpm"
+
+# define FLOOR_IMG "./textures/floor/floor.xpm"
+
+# define C_IMG_0 "./textures/coins/coin1.xpm"
+# define C_IMG_1 "./textures/coins/coin2.xpm"
+# define C_IMG_2 "./textures/coins/coin3.xpm"
+# define C_IMG_3 "./textures/coins/coin4.xpm"
+# define C_IMG_4 "./textures/coins/coin55.xpm"
+# define C_IMG_5 "./textures/coins/coin6.xpm"
+
+# define EXIT_IMG "./textures/exit/color.xpm"
+
+# define WALL_IMG	"./textures/walls/wall.xpm"
+# define RTLB		"./textures/walls/wall_rtlb.xpm"
+# define RBL		"./textures/walls/wall_rbl.xpm"
+# define LTB		"./textures/walls/wall_ltb.xpm"
+# define RTL		"./textures/walls/wall_rtl.xpm"
+# define RTB		"./textures/walls/wall_rtb.xpm"
+# define RL			"./textures/walls/wall_rl.xpm"
+# define TB			"./textures/walls/wall_tb.xpm"
+# define RB			"./textures/walls/wall_rb.xpm"
+# define LB			"./textures/walls/wall_lb.xpm"
+# define RT			"./textures/walls/wall_rt.xpm"
+# define LT			"./textures/walls/wall_lt.xpm"
+# define B			"./textures/walls/wall_b.xpm"
+# define T			"./textures/walls/wall_t.xpm"
+# define R			"./textures/walls/wall_r.xpm"
+# define L			"./textures/walls/wall_l.xpm"
+
+#endif // TEXTURES_H
