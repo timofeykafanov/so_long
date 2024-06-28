f5ee433327006ff539d8ddcc49897b2462acb5df
timofeykafanov <tim.kafanov@gmail.com>
2024-06-28 22:54:01 +0200

    refactor: modify enemies moves

--- srcs/sprite_enemy_bonus.c
+++ srcs/sprite_enemy_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tkafanov <tkafanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:17:33 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/06/28 14:16:56 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/06/28 22:14:07 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,16 @@ static void	sprite_red(t_mlx_data data, int y, int x, int frame)
 static void	sprite_purple(t_mlx_data data, int y, int x, int frame)
 {
 	if (frame < 3)
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_9, \
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_10, \
 			x * data.img.w, y * data.img.h + 45);
 	else if (frame < 6)
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_11, \
-			x * data.img.w, y * data.img.h + 45);
-	else if (frame < 9)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_9, \
 			x * data.img.w, y * data.img.h + 45);
+	else if (frame < 9)
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_11, \
+			x * data.img.w, y * data.img.h + 45);
 	else
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_10, \
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_9, \
 			x * data.img.w, y * data.img.h + 45);
 }
 
@@ -50,29 +50,29 @@ static void	sprite_sky(t_mlx_data data, int y, int x, int frame)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_6, \
 			x * data.img.w, y * data.img.h + 45);
 	else if (frame < 6)
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_8, \
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_7, \
 			x * data.img.w, y * data.img.h + 45);
 	else if (frame < 9)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_6, \
 			x * data.img.w, y * data.img.h + 45);
 	else
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_7, \
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_8, \
 			x * data.img.w, y * data.img.h + 45);
 }
 
 static void	sprite_green(t_mlx_data data, int y, int x, int frame)
 {
 	if (frame < 3)
-		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_3, \
-			x * data.img.w, y * data.img.h + 45);
-	else if (frame < 6)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_5, \
 			x * data.img.w, y * data.img.h + 45);
-	else if (frame < 9)
+	else if (frame < 6)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_3, \
 			x * data.img.w, y * data.img.h + 45);
-	else
+	else if (frame < 9)
 		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_4, \
+			x * data.img.w, y * data.img.h + 45);
+	else
+		mlx_put_image_to_window(data.mlx, data.wdw, data.img.enemy_3, \
 			x * data.img.w, y * data.img.h + 45);
 }
 
